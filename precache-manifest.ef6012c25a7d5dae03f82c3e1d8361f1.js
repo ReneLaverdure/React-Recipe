178ea648626761e923658fecec00384341cc726e
Rene Laverdure <renelaverdure@gmail.com>
2019-09-17 20:10:41 +1000

    Updates

---
+++ precache-manifest.ef6012c25a7d5dae03f82c3e1d8361f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b61bf522ead86db4ed5751eda8f0e24",
+    "url": "/react-recipe/index.html"
+  },
+  {
+    "revision": "bb97add67411b9070aba",
+    "url": "/react-recipe/static/css/main.ea54eb7c.chunk.css"
+  },
+  {
+    "revision": "ff4492c38a5e26df4e39",
+    "url": "/react-recipe/static/js/2.43458225.chunk.js"
+  },
+  {
+    "revision": "bb97add67411b9070aba",
+    "url": "/react-recipe/static/js/main.84b0abed.chunk.js"
+  },
+  {
+    "revision": "ac0fc24412ed626408ec",
+    "url": "/react-recipe/static/js/runtime~main.31912138.js"
+  },
+  {
+    "revision": "aa3a35c9b0e8510cb3d003cbaae594cd",
+    "url": "/react-recipe/static/media/food-5.aa3a35c9.jpg"
+  }
+]);
